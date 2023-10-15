03188e82bff4acaf65c3253284546e28d7bf0f0a
Guilherme <guilhermeandriel78@gmail.com>
2023-10-15 20:41:18 -0300

    DOCS: adicionando documentos

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Seleção de elementos 
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+
+const multiplicationTable = document.querySelector("#multiplication-operations")
+const multiplicationTitle = document.querySelector("#multipliation-title span")
+
+//Funções 
+const createTable = (number, multiplicatorNumber) =>{
+    console.log(number, multiplicatorNumber)
+    multiplicationTable.innerHTML = ""
+    
+    for(let i = 0; i<=multiplicatorNumber; i++){
+        const result = number *i;
+
+    const template = `<div class="row">
+        <div class="operation">${number} x ${i} = </div>
+        <div class="result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser()
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row")
+        multiplicationTable.appendChild(row)
+    }
+
+    multiplicationTitle.innerText = number;
+}
+
+//Eventos
+multiplicationForm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const multiplicationNumber = +numberInput.value
+
+    const multiplicatorNumber = +multiplicationInput.value
+
+    if(!multiplicationNumber || !multiplicatorNumber)return;
+
+    createTable(multiplicationNumber, multiplicatorNumber)
+
+    
+
+})
